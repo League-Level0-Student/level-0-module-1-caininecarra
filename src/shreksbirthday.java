febff7b6c4eb9ab1feb543e79fc1be85b87f8378
Cainnan Carra <league@wts-im-6.attlocal.net>
2018-04-22 15:59:11 -0700

    shreks birtday

---
+++ src/shreksbirthday.java
@@ -0,0 +1,11 @@
+import javax.swing.JOptionPane;
+
+public class shreksbirthday { public static void main(String[] args) {
+String birthday=	JOptionPane.showInputDialog ("whats your birthday?");
+if(birthday.equals("april 22")) {
+	null
+	
+}
+}
+
+}
