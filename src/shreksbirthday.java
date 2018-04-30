6e4727808659fea5565308457c25bccdc4903a76
Cainnan Carra <league@WTS10.attlocal.net>
2018-04-30 16:54:44 -0700

    shrek bday

--- src/shreksbirthday.java
+++ src/shreksbirthday.java
@@ -1,10 +1,11 @@
 import javax.swing.JOptionPane;
 
-public class shreksbirthday { public static void main(String[] args) {
+public class shreksbirthday {
+	public static void main(String[] args) {
 String birthday=	JOptionPane.showInputDialog ("whats your birthday?");
-if(birthday.equals("april 22")) {
-	null
-	
+if(birthday.equals("04/30")) {
+	JOptionPane.showMessageDialog(null, "happy birthday");
+	else 
 }
 }
 
