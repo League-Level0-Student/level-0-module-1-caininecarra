b72194617477803796029459d151a5cc51cef362
Cainnan Carra <league@WTS-IM-6.attlocal.net>
2018-06-03 15:58:21 -0700

    R

---
+++ src/Remarkable.java
@@ -0,0 +1,44 @@
+import javax.swing.JOptionPane;
+
+public class Remarkable { public static void main(String[] args) {
+	
+	String shrek="shrek lives in a swamp";
+	String mike="mike is a stupid roadhog main that i have to go reaper to counter";
+	String kaiya="i hate her honestly";
+	String rohan="hes really good at fortnite";
+	String adi="i like his phone case";
+	String nik="i dont know him at all";
+	String elijah="um";
+	String cainine="a mercy main that gets 20k healing per game";
+	
+	String name=JOptionPane.showInputDialog ("whats your name");
+	if (name.equals("shrek")) {
+	JOptionPane.showMessageDialog(null, shrek);
+	 
+	}
+	if (name.equals("mike")) {
+	JOptionPane.showMessageDialog(null, mike); 
+	}
+	if (name.equals("kaiya")) {
+	JOptionPane.showMessageDialog(null, kaiya);
+	}
+	if (name.equals("rohan")) {
+	JOptionPane.showMessageDialog(null, rohan);
+	}
+	if (name.equals("adi")) {
+	JOptionPane.showMessageDialog(null, adi);
+	}
+	if (name.equals("nik")) {
+	JOptionPane.showMessageDialog(null, nik);
+	}
+	if (name.equals("elijah")) {
+	JOptionPane.showMessageDialog(null, elijah);
+
+	if (name.equals("cainine")) {
+	JOptionPane.showMessageDialog(null, "cainine");
+	}
+	
+	}
+	
+}
+}
